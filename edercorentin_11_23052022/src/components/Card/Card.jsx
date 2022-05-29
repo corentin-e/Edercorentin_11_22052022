77489cf5cb5ca66ea3b6dc00e100117f4f28e871
Etha <76168237+etharion29@users.noreply.github.com>
2022-05-29 13:23:12 +0200

    Home page finishing

--- edercorentin_11_23052022/src/components/Card/Card.jsx
+++ edercorentin_11_23052022/src/components/Card/Card.jsx
@@ -1,9 +1,10 @@
 import "./card.css";
 
-const Card = () => (
+const Card = ({title, image_source}) => (
   <div className="card_background">
+    <img className="card_image" src={image_source} alt="Housing_image"/>
     <div className="card_items_position">
-      <label className="card_item_position">Titre de la location</label>
+      <label className="card_text_position">{title}</label>
     </div>
   </div>
 );
--- edercorentin_11_23052022/src/pages/Home/Home.js
+++ edercorentin_11_23052022/src/pages/Home/Home.js
@@ -1,13 +1,17 @@
 import "./home.css";
 import { Banner, Card, DropdownList, DropdownInfo, Tag, Header } from "../../components";
+import data from "../../data.json"
+import React from "react";
 
 function Home() {
   return (
     <div className="homePage_items_position">
       <Header />
       <Banner />
-      <div className="homePage_housing_background">
-        <Card/>
+      <div className="homePage_housing_position">
+      {data.housings.map(housing => (
+        <Card key={housing.id} title={housing.title} image_source={housing.cover}/>
+      ))}
       </div>
     </div>
   );
