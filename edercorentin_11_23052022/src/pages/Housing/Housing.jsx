3c6e0f9072f4c8865602e637e57a80609dc84aa2
Etha <76168237+etharion29@users.noreply.github.com>
2022-06-06 16:23:43 +0200

    Link housing items by ID is fixed

--- edercorentin_11_23052022/src/pages/Housing/Housing.jsx
+++ edercorentin_11_23052022/src/pages/Housing/Housing.jsx
@@ -1,20 +1,21 @@
 import "./housing.css";
-import { Header, Footer, Carrousel } from "../../components";
+import { Header, Footer, /* Carrousel  */} from "../../components";
 import data from "../../data.json"
 import React from "react";
+import { useParams } from 'react-router-dom';
 
 const Housing = () =>  {
+    const { id } = useParams();
+    console.log(id)
 
-    const images = data.housings.map(housing => (housing.pictures));
-    console.log(images)
+    const housingId = data.housings.find(housing => housing.id === id)
+    console.log(housingId)
 
   return (
-    <div className="housingPage_items_position">
+    <div className="housingPage_page_position">
       <Header />
       <div className="housingPage_items_position">
-      {data.housings.map(housing => (
-          <Carrousel image_source={housing.pictures}/>
-      ))}
+        <div className="housingPage_title_housin">{housingId.title}</div>
       </div>
       <div className="housingPage_footer_position"/>
         <Footer/>
